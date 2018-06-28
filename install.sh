28f01432f41195c53d21beac51ea6a33bbeb357b
ZeroByte.ID Warehouse <38853171+zerobyte-id@users.noreply.github.com>
2018-06-28 14:59:58 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,14 @@
+#!/data/data/com.termux/files/usr/bin/bash
+
 pkg install mlocate;
+pkg install curl;
+pkg upgrade curl;
 updatedb
 cp -r ../0byttt-whackdroid/ /data/data/com.termux/files/usr/share/
 mv /data/data/com.termux/files/usr/share/0byttt-whackdroid /data/data/com.termux/files/usr/share/whackdroid 
 echo "#!/data/data/com.termux/files/usr/bin/bash" >> /data/data/com.termux/files/usr/bin/whackdroid
 echo "/data/data/com.termux/files/usr/share/whackdroid/whackdroid.bash" >> /data/data/com.termux/files/usr/bin/whackdroid
-chmod +x /data/data/com.termux/files/usr/share/whackdroid/whackdroid.bash
+chmod -R +x /data/data/com.termux/files/usr/share/whackdroid/
+mv /data/data/com.termux/files/usr/share/whackdroid/whackdroid-update.bash /data/data/com.termux/files/usr/share/whackdroid/whackdroid-update
 chmod +x /data/data/com.termux/files/usr/bin/whackdroid
+chmod +x /data/data/com.termux/files/usr/share/whackdroid/whackdroid-update
