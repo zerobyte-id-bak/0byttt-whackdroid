3439cc065ae130bc6bb598d5b251adc2dce59f80
ZeroByte.ID Warehouse <38853171+zerobyte-id@users.noreply.github.com>
2018-06-28 17:12:43 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -9,6 +9,6 @@ mv /data/data/com.termux/files/usr/share/0byttt-whackdroid /data/data/com.termux
 echo "#!/data/data/com.termux/files/usr/bin/bash" >> /data/data/com.termux/files/usr/bin/whackdroid
 echo "/data/data/com.termux/files/usr/share/whackdroid/whackdroid.bash" >> /data/data/com.termux/files/usr/bin/whackdroid
 chmod -R +x /data/data/com.termux/files/usr/share/whackdroid/
-mv /data/data/com.termux/files/usr/share/whackdroid/whackdroid-update.bash /data/data/com.termux/files/usr/share/whackdroid/whackdroid-update
+mv /data/data/com.termux/files/usr/share/whackdroid/whackdroid-update.bash /data/data/com.termux/files/usr/bin/whackdroid-update
 chmod +x /data/data/com.termux/files/usr/bin/whackdroid
-chmod +x /data/data/com.termux/files/usr/share/whackdroid/whackdroid-update
+chmod +x /data/data/com.termux/files/usr/bin/whackdroid-update
